347ba7a8f61a57aabb46e6b736feba71c8f9a34d
Ankita Doshi <ankita.doshi@sap.com>
2023-07-12 20:21:36 +0530

    Checkin for display name

--- github-cards/src/app/components/Card/Card.tsx
+++ github-cards/src/app/components/Card/Card.tsx
@@ -2,16 +2,25 @@ import React from "react";
 import styles from "./card.module.css";
 
 export default class Card extends React.Component {
+    state = {
+        userName:''
+    }
+   handleCardClick = async (evt: any) => {
+        const { user } = evt.target.dataset;
+        const resp = await fetch(`https://api.github.com/users/${user}`);
+        const userResp = await resp.json();
+        this.setState({userName: userResp.name || 'test'});
+    }
     render() {
        const user: any = this.props;
         return (
-            <div className={styles.container}>
+            <div data-user={user.login} className={styles.container} onClick={this.handleCardClick}>
                 <img src={user.avatar_url} className={styles.image}></img>
                 <div className={styles.info}>
                     <span className={styles.label}>Name:</span>
                     <div className={styles.name}>{user.login}</div>
                 </div>
-                <i ></i>
+                <i>{this.state.userName}</i>
             </div>
         )
     }
